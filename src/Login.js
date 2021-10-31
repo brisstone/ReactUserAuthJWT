06e29ba06ff02a4a9d648dcbc7fd2eb58e2b0f37
brisstone <okolijohnson69@gmail.com>
2021-10-31 14:35:48 +0100

    make it better

--- src/Login.js
+++ src/Login.js
@@ -48,7 +48,7 @@ function Login(props) {
 
   return (
     <div>
-      Login Here<br /><br />
+      Login here<br /><br />
       <div>
         Username<br />
         <input type="text" {...username} autoComplete="new-password" />
